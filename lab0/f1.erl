f2768656051af1e2e3ca7edefb12a18a80086dbe
Jaśko <jan.palucki@gmail.com>
2016-10-06 23:20:51 +0200

    Example of tricky Erland code

--- lab0/f1.erl
+++ lab0/f1.erl
@@ -4,3 +4,8 @@
 function(N) -> N*4.
 
 %Firstly, insert into erlang environment. Compile with: c(module_name). Execute with module_name:function_name(arguments).
+
+% tricky Erlang stuff: 
+% in compiler type '[65,66,67,68,69].'.
+% It should print the same: list of numbers.
+% IT SHOULD. But....
