923d0e6b8818e32827562404876b51448c4cea87
Jaśko <jan.palucki@gmail.com>
2016-10-27 14:07:00 +0200

    Changed using of hardcoded function to one given by user

--- lab3/mapa.erl
+++ lab3/mapa.erl
@@ -2,27 +2,23 @@
 
 -compile(export_all).
 
+wrapper(Parent, Function, Arg) ->
+	Parent ! Function(Arg).
 
-exFunction(Parent, Arg) ->
-	Parent ! {Arg*3}.
-
-procMap([]) -> [];
-
-procMap([Head|Tail]) ->
-	RefTag = make_ref(),
-	spawn(?MODULE, exFunction, [self(), Head]),
-	procMap(Tail) ++
-	receive
-		{Result} -> [Result]
-	end.
+procMap([Head|Tail], Function) ->
+	%RefTag = make_ref(),
+	spawn(?MODULE, wrapper, [self(), Function, Head]),
+	procMap(Tail, Function);
 	
-start(List) ->
-	lists:reverse(procMap(List)).
+procMap([], _) -> collectResults().
+
+collectResults() ->
+	receive 
+		Result -> 
+			[Result] ++ collectResults()
+	after 200 ->
+			[]
+	end.
 
 
-
-%%mapa ronwoległa (zwracane wyniki mogą byc nie po kolei - uzyc mk_ref())
-
-% Jakby chcieć brać funkcje od użytkownika to jak wywołać spawn (w wersji 1 argumentowej nie ma jak podać parametrów, a w wersji 3 argumentowej nie wiadomo jaki moduł)
-
-% Czy obecna wersja jest współbieżna (Jakby powywoływać spawna dla każdego argumentu i w każdym byłoby receive to jak to potem połączyć z powrotem do kupy?)
+%% TODO:: zwracane wyniki mogą byc nie po kolei - uzyc mk_ref())
