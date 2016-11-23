e2ac919abb1a42cac351353ab59824d378f83a0f
Jaśko <jan.palucki@gmail.com>
2016-11-23 14:05:08 +0100

    Extracted saving state to separate command, added recreating server state

--- lab6/mod.erl
+++ lab6/mod.erl
@@ -4,18 +4,42 @@
 
 loop(Parent,List)->
 	receive
-		{store, Name} -> 
+		saveState ->
 			R = io_lib:format("~p",[self()]),
 			lists:flatten(R),
 			String=lists:nth(1,R),
 			Filename= string:substr(String,2,length(String)-2)++".txt",
 			{ok,FD} = file:open(Filename,[write]),
 			
-			S = io_lib:format("~p", [lists:append([Name],List)] ),
+			S = io_lib:format("~p", [List] ),
 			lists:flatten(S),
-			StringName = lists:nth(1,S),
-			file:write(FD, StringName),
+			StringList = lists:nth(1,S),
+			file:write(FD, StringList),
 			file:close(FD),
+			Parent ! {savedState},
+			loop(Parent,List);
+
+		{recreateState,PID} ->
+			R = io_lib:format("~p",[PID]),
+			lists:flatten(R),
+			String=lists:nth(1,R),
+			Filename= string:substr(String,2,length(String)-2)++".txt",
+			case filelib:is_file(Filename) of
+				true -> 
+					{ok,FD} = file:open(Filename,[read]),
+					{ok, Content} = file:read_line(FD),
+					StringList = string:substr(Content,2,string:len(Content)-2),
+					io:format(StringList),
+					StringStatesList = string:tokens(StringList, ","),
+					StatesList = lists:map(fun(X)->list_to_atom(X) end,StringStatesList),
+					Parent ! {recreated_state},
+					loop(Parent,StatesList);
+				false -> 
+					Parent ! {state_for_PID_not_found},
+					loop(Parent,List)
+			end;
+			
+		{store, Name} -> 
 			Parent ! {stored},
 			loop(Parent,[Name|List]);
 			
