8d5fc61506603c2f2cde646181ec5adb95ab3ac5
Jaśko <jan.palucki@gmail.com>
2016-11-23 12:30:13 +0100

    Saving a server state to a text file

--- lab6/mod.erl
+++ lab6/mod.erl
@@ -5,8 +5,20 @@
 loop(Parent,List)->
 	receive
 		{store, Name} -> 
+			R = io_lib:format("~p",[self()]),
+			lists:flatten(R),
+			String=lists:nth(1,R),
+			Filename= string:substr(String,2,length(String)-2)++".txt",
+			{ok,FD} = file:open(Filename,[write]),
+			
+			S = io_lib:format("~p", [lists:append([Name],List)] ),
+			lists:flatten(S),
+			StringName = lists:nth(1,S),
+			file:write(FD, StringName),
+			file:close(FD),
 			Parent ! {stored},
 			loop(Parent,[Name|List]);
+			
 		{delete, Name} -> 
 			case isPresent(Name,List) of
 				true ->
@@ -16,6 +28,7 @@ loop(Parent,List)->
 					Parent ! {not_found},
 					loop(Parent,List)
 			end;
+			
 		{isPresent, Name} ->
 			case isPresent(Name,List) of
 				true ->
@@ -25,7 +38,8 @@ loop(Parent,List)->
 					Parent ! {not_found},
 					loop(Parent, List)
 			end;
-		{exit} ->
+			
+		_ ->
 			io:format("Server stopped ")
 	end.
 	
