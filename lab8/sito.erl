03270010fa8bdd3edc6de1c90cf3df9c04e0d494
Jaśko <jan.palucki@gmail.com>
2017-01-24 11:50:51 +0100

    Fixed bugs for small numbers

--- lab8/sito.erl
+++ lab8/sito.erl
@@ -8,22 +8,34 @@ start(N) ->
 	if 
 		is_integer(N) and (N > 2) -> 	
 			List = lists:seq(3,N,2),
-			BasicPrimes = findPrimes([2]++lists:seq(3,round(math:sqrt(N)),2)),
-			ListToCheck = lists:sublist(List,length(BasicPrimes)+1,length(List)-length(BasicPrimes)),
+			BasicPrimes = findPrimes([2] ++ lists:seq(3,round(math:sqrt(N)),2)),
+			ListToCheck = lists:sublist(List, length(BasicPrimes), length(List)-length(BasicPrimes)+1),
 			try spawnProcesses(BasicPrimes, ListToCheck)
 			catch
-				_:_ -> start(N)
+				_:_ -> exit
 			end;
+		N == 2 -> [2];
+		N == 1 -> [];
 		true ->
-			io:format("Nalezy podac liczbe calkowita wieksza od 2~n")
+			io:format("Nalezy podac liczbe calkowita wieksza od 0~n")
 	end.
 
 spawnProcesses(BasicPrimes, ListToCheck) ->
 	T1 = erlang:timestamp(),
 	Length = length(ListToCheck),
 	NumberOfThreads = erlang:system_info(logical_processors_available) * ((Length div 1000000)+1),
-	PartLen = round(Length/NumberOfThreads),
-	Refs = spawnProcess(NumberOfThreads, NumberOfThreads, [], BasicPrimes, ListToCheck, PartLen),
+	if
+		round(Length/NumberOfThreads) > 0 ->
+			PartLen = round(Length/NumberOfThreads);
+		true ->
+			PartLen = 1
+	 end,
+	if 
+		(NumberOfThreads > Length) ->
+			Refs = spawnProcess(Length, Length, [], BasicPrimes, ListToCheck, PartLen);
+		true ->
+			Refs = spawnProcess(NumberOfThreads, NumberOfThreads, [], BasicPrimes, ListToCheck, PartLen)
+	end,
 	T2 = erlang:timestamp(),
 	io:format("Obliczenia trwaly ~p mikrosekund~n",[timer:now_diff(T2,T1)]),
 	BasicPrimes ++ collectResults(Refs).
@@ -32,7 +44,8 @@ spawnProcess(_, 0, ListOfRefs, _, _, _) -> ListOfRefs;
 	
 spawnProcess(NumberOfThreads, Iter, ListOfRefs, BasicPrimes, ListToCheck, PartLen) ->
 	Ref = make_ref(),
-	spawn_link(?MODULE, filterPart, [self(), Ref, BasicPrimes, lists:sublist(ListToCheck, (NumberOfThreads-Iter)*PartLen+1, PartLen)]),
+	Sublist = lists:sublist(ListToCheck, (NumberOfThreads-Iter)*PartLen+1, PartLen),
+	spawn_link(?MODULE, filterPart, [self(), Ref, BasicPrimes, Sublist]),
 	spawnProcess(NumberOfThreads, Iter-1, ListOfRefs++[Ref], BasicPrimes, ListToCheck, PartLen).
 	
 collectResults([Ref|Tail]) ->
